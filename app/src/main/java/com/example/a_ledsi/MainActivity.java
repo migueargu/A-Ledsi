52bdb80df616af801941b891200b11a281f601a0
miguel argueta <miguelargueta2351@hotmail.com>
2023-11-28 02:46:28 -0600

    new

--- app/src/main/java/com/example/a_ledsi/MainActivity.java
+++ app/src/main/java/com/example/a_ledsi/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.a_ledsi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onLoad();
     }
+
+    private void onLoad(){
+        final int TIMER = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        }, TIMER);
+    }
+
 }
