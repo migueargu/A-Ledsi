38b51093d497b0c5764f24627f66bc8586b7b653
miguel argueta <miguelargueta2351@hotmail.com>
2023-11-28 02:47:49 -0600

    new

---
+++ app/src/main/java/com/example/a_ledsi/login.java
@@ -0,0 +1,20 @@
+package com.example.a_ledsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+
+
+}
